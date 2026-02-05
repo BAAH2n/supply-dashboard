3af7dad31185889218f156f34a8255a99b7b0dfe
BAAH2n <arsen.h.2007@gmail.com>
2026-02-05 22:11:43 +0200

    Final

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import glob 
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -8,21 +9,34 @@ load_dotenv()
 token = os.getenv("SLACK_BOT_TOKEN")
 channel_id = os.getenv("CHANNEL_ID")
 
+image_folder = "images"
+
+search_pattern = os.path.join(image_folder, "*.png") 
+list_of_files = glob.glob(search_pattern) 
+
+if not list_of_files:
+    print(f"Помилка: У папці '{image_folder}' не знайдено жодного PNG файлу!")
+    exit()
+    
+latest_file = max(list_of_files, key=os.path.getmtime)
+
+print(f"✅ Знайдено найновіше зображення: {latest_file}")
 
 if not token:
-    print("Помилка: Токен не знайдено. Перевірте файл .env")
+    print("Помилка: Токен не знайдено.")
 else:
     client = WebClient(token=token)
-    
+
 try:
-  client.files_upload_v2(
-    channel = channel_id,
-    file = "Rplot.png",
-    title = "Нова інформація по метрикам!!",
-    initial_comment="Текст повідомлення, який піде разом з картинкою"
-)
+    response = client.files_upload_v2(
+        channel=channel_id,
+        file=latest_file, 
+        title="Свіжий звіт",
+        initial_comment=f"Ось найактуальніший дашборд ({os.path.basename(latest_file)})"
+    )
+    print("Зображення успішно надіслано!")
+
 except SlackApiError as e:
-  print(f"Error uploading file: {e.response['error']}")
-else:
-  print("Зображення надіслано!")
-  
+    print(f"Error uploading file: {e.response['error']}")
+except FileNotFoundError:
+    print(f"Файл {latest_file} кудись зник під час роботи скрипта.")
